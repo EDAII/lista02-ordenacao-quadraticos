4d6ce7d8ba051c8d0f82c6b4a9d4c1df1f1a527f
jppgomes <joaok8@gmail.com>
2018-08-29 08:25:34 -0300

    ADD: time to insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,4 +1,6 @@
-# import random
+import random
+import time
+
 def insertion_sort(vector):
     for i in range(len(vector)):
         key = vector[i]
@@ -8,14 +10,28 @@ def insertion_sort(vector):
             j -= 1
         vector[j + 1] = key
 
-# lista = list()
 
-# for i in range(10):
-#     lista.append(random.randint(0,1000))
-#     print(lista[i])
+vector = list(range(0,100))
+random.shuffle(vector) #embaralha os vetores
 
-# print('-----')
 
-# insertion_sort(lista)
-# for i in range(len(lista)):
-#     print(lista[i])
+before = time.time()
+insertion_sort(vector)
+after = time.time()
+
+total  = (after-before)*1000
+
+print("Tempo de ordenação para vetor de 100 elementos: %0.2f" %total)
+
+
+vector2 = list(range(0,1000))
+random.shuffle(vector2)
+
+before2 = time.time()
+insertion_sort(vector2)
+after2 = time.time()
+
+total2 = (after2-before2)*1000
+
+
+print("Tempo de ordenação para vetor de 1000 elementos: %0.2f" %total2)
